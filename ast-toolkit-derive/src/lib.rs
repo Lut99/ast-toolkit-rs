c44f7072083a6073943fa7192e8c5e8dd9565437
Lut99 <mullertim99@gmail.com>
2023-07-05 15:48:08 +0200

    Added boilerplate for second derive crate

---
+++ ast-toolkit-derive/src/lib.rs
@@ -0,0 +1,16 @@
+//  LIB.rs
+//    by Lut99
+// 
+//  Created:
+//    05 Jul 2023, 15:47:40
+//  Last edited:
+//    05 Jul 2023, 15:47:50
+//  Auto updated?
+//    Yes
+// 
+//  Description:
+//!   Crate that implements various `#[derive(...)]` macros for the
+//!   `ast-toolkit` crate.
+// 
+
+
