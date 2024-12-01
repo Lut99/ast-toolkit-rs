1ced7b686157e7516a07f579c067f8116cd69448
Lut99 <mullertim99@gmail.com>
2024-12-01 12:21:21 +0100

    Added all `debug`-combinators

--- lib/ast-toolkit-snack/src/debug2/mod.rs
+++ lib/ast-toolkit-snack/src/debug2/mod.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    30 Nov 2024, 22:06:33
 //  Last edited:
-//    30 Nov 2024, 22:07:11
+//    01 Dec 2024, 12:17:59
 //  Auto updated?
 //    Yes
 //
@@ -14,9 +14,9 @@
 //
 
 // Declare modules
-// pub mod inspect;
-// pub mod inspect_after;
+pub mod inspect;
+pub mod inspect_after;
 
 // Use some of it
-// pub use inspect::inspect;
-// pub use inspect_after::inspect_after;
+pub use inspect::inspect;
+pub use inspect_after::inspect_after;
