c5fc0a926eeaab1bd377e28ab350a6f65fdad395
Lut99 <mullertim99@gmail.com>
2025-10-10 11:44:24 +0200

    Fixed macros to work with re-export of `console`'s `Style`, whoops.

--- lib/ast-toolkit-print/src/macros.rs
+++ lib/ast-toolkit-print/src/macros.rs
@@ -20,23 +20,22 @@ macro_rules! write_dedup {
 
 
 /// A macro for conveniently calling
-/// [`Style::apply_to_args()`](crate::formatter::Style::apply_to_args()) and writing it to a
-/// formatter.
+/// [`Style::apply_to()`](crate::formatter::Style::apply_to()) and writing it to a formatter.
 #[cfg(feature = "color")]
 #[macro_export]
 macro_rules! write_styled {
     ($f:expr, $style:expr, $($t:tt)*) => {
-        ::std::write!($f, "{}", <$crate::Style>::apply_to_args($style, ::std::format_args!($($t)*)))
+        ::std::write!($f, "{}", <$crate::Style>::apply_to($style, ::std::format_args!($($t)*)))
     };
 }
 
 /// A macro for conveniently calling
-/// [`Style::apply_to_args()`](crate::formatter::Style::apply_to_args()) and writing it to a
-/// formatter with a newline.
+/// [`Style::apply_to()`](crate::formatter::Style::apply_to()) and writing it to a formatter with a
+/// newline.
 #[cfg(feature = "color")]
 #[macro_export]
 macro_rules! writeln_styled {
     ($f:expr, $style:expr, $($t:tt)*) => {
-        ::std::writeln!($f, "{}", <$crate::Style>::apply_to_args($style, ::std::format_args!($($t)*)))
+        ::std::writeln!($f, "{}", <$crate::Style>::apply_to($style, ::std::format_args!($($t)*)))
     };
 }
